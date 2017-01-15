c4d90d50160a992a88332e39811e4fde2d7c9ce9
Maadar <marcin171194@wp.pl>
2017-01-15 18:40:46 +0100

    development process

--- main/js/app.js
+++ main/js/app.js
@@ -19,11 +19,10 @@ app.controller("mainCtrl", function($scope, answersService) {
 	//load data from json and put it to the variable
 	promiseAnswers.then(function(data) {
 		$scope.answer = data;
-		console.log($scope.answer);
 	});
 
 	//array contains values of buttons
-	$scope.letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "W", "Y", "Z"];
+	$scope.letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 	var config = {
 		charCounter : 0,
@@ -31,7 +30,8 @@ app.controller("mainCtrl", function($scope, answersService) {
 		attempts : 6,
 		chosenWord : "",
 		strikedLetter : true,
-		displayHangmanImage : ""
+		displayHangmanImage : "",
+		guessedWords : 0
 	};
 	$scope.config = config;
 
@@ -49,14 +49,11 @@ app.controller("mainCtrl", function($scope, answersService) {
 
 	var buttons = {
 		displayStartBtn : true,
-		displayNextBtn : false
+		displayNextBtn : false,
+		disableClickedButton : true
 	}
 	$scope.buttons = buttons;
 
-	buttons.displayStartBtn;
- 	config.isAlreadyDrawn;
- 	$scope.disableClickedButton = false;
-	
 	String.prototype.setLetter = function(place, character, index) {
 		if (place > this.length -1) return this.toString(); //check length and protect against check letter, which doesn't exist and be sure it is converted on string
 		else return this.substr(0, place) + character + this.substr(place + 1);
@@ -68,6 +65,7 @@ app.controller("mainCtrl", function($scope, answersService) {
 		isAlreadyDrawn(); 
 		convertString();
 		buttons.displayNextBtn = true;
+		buttons.disableClickedButton = false;
 	}
 
 	$scope.next = function() {
@@ -81,37 +79,16 @@ app.controller("mainCtrl", function($scope, answersService) {
 	//check exist letters in string and switch "-" on letter
 	$scope.check = function(index, letter) {
 		config.strikedLetter = true;
-		console.log($scope.letters[letter, index]); //contain clicked letter
 		//iterate via each letter and check if this letter is equal to the button value
 		for (i=0; i<config.charCounter; i++) { //charCounter contains int
 			if (config.chosenWord.jsonVal.charAt(i) == $scope.letters[letter, index]) { //letter contains value of the button, index contains correct number of the button
 				convert.hiddenPassword = convert.hiddenPassword.setLetter(i, letter, index);
 				config.strikedLetter = false;
-				console.log(convert.hiddenPassword);
 			}
 		}
-
-		if (config.strikedLetter == true) {
-			config.attempts = config.attempts - 1;
-			this.disableClickedButton = true;
-			
-		// display images when letter is missed
-		if 		(config.attempts == 5) { config.displayHangmanImage = $scope.answer[1].img; } 
-		else if (config.attempts == 4) { config.displayHangmanImage = $scope.answer[2].img; }
-		else if (config.attempts == 3) { config.displayHangmanImage = $scope.answer[3].img; }
-		else if (config.attempts == 2) { config.displayHangmanImage = $scope.answer[4].img; }
-		else if (config.attempts == 1) { config.displayHangmanImage = $scope.answer[5].img; }
-		else if (config.attempts == 0) { config.displayHangmanImage = $scope.answer[6].img; }
-
-		} else this.disableClickedButton = true;
+		isLetterStriked();	
+		attemptsResult();
 		finishAlert();
-		//info about defeat
-		if (config.attempts == 0){
-			console.log("Przegrales");
-			$scope.disableClickedButton = true;
-			alert.showDefeatAlert = false;
-		} 
-		if ((config.attempts !=0) && (convert.hiddenPassword == config.chosenWord.jsonVal)) alert.showSuccessAlert = false;
 	}
 
 	//convert string on password
@@ -124,27 +101,21 @@ app.controller("mainCtrl", function($scope, answersService) {
 			? convert.hiddenPassword = convert.hiddenPassword + " "
 			: convert.hiddenPassword = convert.hiddenPassword + "-"
 		}
-		console.log(convert.hiddenPassword); 
 	}			
 
 	//function checks if word is already drawn. If true, push to the array and dont draw it again
 	var isAlreadyDrawn = function () {
 		if (config.isAlreadyDrawn.indexOf(config.chosenWord.jsonVal) == -1) {
 			config.isAlreadyDrawn.push(config.chosenWord.jsonVal);
-			console.log(config.isAlreadyDrawn);
 		} 
-		else {
+		else 
+		{
 			do {
-				console.log(config.chosenWord.jsonVal);
 				for (i = 0; i <= config.isAlreadyDrawn.length; i++) {
 					config.chosenWord = $scope.answer[Math.floor(Math.random() * $scope.answer.length)];
-					if (config.isAlreadyDrawn[i] == config.chosenWord.jsonVal) {
-					    console.log(config.chosenWord.jsonVal);
-					}
 				}
 			} while (config.isAlreadyDrawn.indexOf(config.chosenWord.jsonVal) != -1);
-		config.isAlreadyDrawn.push(config.chosenWord.jsonVal);
-		console.log(config.isAlreadyDrawn);
+			config.isAlreadyDrawn.push(config.chosenWord.jsonVal);
 		}
 		finishAlert();
 	}
@@ -153,18 +124,45 @@ app.controller("mainCtrl", function($scope, answersService) {
 	var finishAlert = function() {
 		if (config.isAlreadyDrawn.length == 6) {
 			buttons.displayNextBtn = false;
-			if (convert.hiddenPassword == config.chosenWord.jsonVal) {
-				console.log("display modal with info"); // modal will be displayed 
+			if ((config.attempts == 0) || (convert.hiddenPassword == config.chosenWord.jsonVal)){
+				window.alert("Game finished! You earned " + config.guessedWords + " points");
 			}
 		}
 	}
-
+	
 	var configuration = function() {
 		config.attempts = 6;
-		if (config.attempts == 6) { config.displayHangmanImage = $scope.answer[0].img; } 
+		(config.attempts == 6) ? config.displayHangmanImage = $scope.answer[0].img : "";
 		config.chosenWord = $scope.answer[Math.floor(Math.random() * $scope.answer.length)];
-		console.log(config.chosenWord.jsonVal);
-		$scope.disableClickedButton = false;
+		buttons.disableClickedButton = false;
 		buttons.displayStartBtn = false;
+	}
+
+	// function responsible for display images with hangman elements
+	var isLetterStriked = function() {
+		if (config.strikedLetter == true) {
+			config.attempts = config.attempts - 1;
+			
+		// display images when letter is missed
+		if 	(config.attempts == 5) { config.displayHangmanImage = $scope.answer[1].img; } 
+		else if (config.attempts == 4) { config.displayHangmanImage = $scope.answer[2].img; }
+		else if (config.attempts == 3) { config.displayHangmanImage = $scope.answer[3].img; }
+		else if (config.attempts == 2) { config.displayHangmanImage = $scope.answer[4].img; }
+		else if (config.attempts == 1) { config.displayHangmanImage = $scope.answer[5].img; }
+		else if (config.attempts == 0) { config.displayHangmanImage = $scope.answer[6].img; }
+
+		}
+	}
+
+	// display alert, depend, which if is executed
+	var attemptsResult = function() {
+		if (config.attempts == 0){
+			buttons.disableClickedButton = true;
+			alert.showDefeatAlert = false;
+		} 
+		if ((config.attempts !=0) && (convert.hiddenPassword == config.chosenWord.jsonVal)) {
+			alert.showSuccessAlert = false;
+			config.guessedWords = config.guessedWords + 1;
+		}
 	}
 });
